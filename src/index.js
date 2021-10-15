d4b09687703cc6032c37adb9af59612b930de92c
Santiago <santiago.arriaga9@gmail.com>
2021-10-15 17:48:41 -0500

    configurar stores para aplicación react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Provider } from 'mobx-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter, Switch, Route } from 'react-router-dom'
@@ -5,18 +6,23 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 import Footer from './footer'
 import Home from './home'
 import ListadoPeliculas from './peliculas/listado'
+import PeliculasStore from './stores/PeliculasStore'
+
+const peliculas = new PeliculasStore()
 
 ReactDOM.render
 (
-  <HashRouter>
-    <div className="container-fluid">
-      <Switch>
-        <Route exact path="/" component={ Home }/>
-	      <Route exact path="/peliculas" component={ ListadoPeliculas }/>
-      </Switch>
-      <Footer/>
-    </div>
-  </HashRouter>
+  <Provider peliculas={ peliculas }>
+    <HashRouter>
+      <div className="container-fluid">
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+          <Route exact path="/peliculas" component={ ListadoPeliculas }/>
+        </Switch>
+        <Footer/>
+      </div>
+    </HashRouter>
+  </Provider>
   ,
   document.getElementById('base')
 )
